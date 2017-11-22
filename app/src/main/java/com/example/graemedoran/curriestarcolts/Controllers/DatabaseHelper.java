53595b2b8bdea6ac55a22eaa94859870d0901240
graeme doran <graeme42@icloud.com>
2017-11-22 11:32:07 +0000

    table refactored

--- app/src/main/java/com/example/graemedoran/curriestarcolts/Controllers/DatabaseHelper.java
+++ app/src/main/java/com/example/graemedoran/curriestarcolts/Controllers/DatabaseHelper.java
@@ -18,6 +18,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_2 = "SQUAD NUMBER";
     public static final String COL_3 = "NAME";
     public static final String COL_4 = "POSITION";
+    public static final String COL_5 = "CONTACT";
+    public static final String COL_6 = "ADDRESS";
+    public static final String COL_7 = "E-MAIL";
+    public static final String COL_8 = "TELEPHONE";
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -25,7 +30,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(" create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, SQUAD_NUMBER INTEGER, NAME TEXT,POSITION TEXT)" );
+        db.execSQL(" create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                " SQUAD_NUMBER INTEGER, NAME TEXT,POSITION TEXT,CONTACT TEXT,ADDRESS TEXT," +
+                "E_MAIL TEXT,TELEPHONE TEXT)" );
     }
 
     @Override
@@ -34,12 +41,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean insertData(String squad_number, String name, String position){
+    public boolean insertData(String squad_number, String name, String position, String contact,
+                              String address, String e_mail, String telephone){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_2,squad_number);
         contentValues.put(COL_3,name);
         contentValues.put(COL_4,position);
+        contentValues.put(COL_5,contact);
+        contentValues.put(COL_6,address);
+        contentValues.put(COL_7,e_mail);
+        contentValues.put(COL_8,telephone);
+
         long result = db.insert(TABLE_NAME,null, contentValues);
         if (result == -1)
             return false;
@@ -52,13 +65,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
         return res;
     }
-    public boolean updateData(String id,String squad_number, String name, String position){
+    public boolean updateData(String id,String squad_number, String name, String position,
+                              String contact, String address, String e_mail, String telephone){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_1,id);
         contentValues.put(COL_2,squad_number);
         contentValues.put(COL_3,name);
         contentValues.put(COL_4,position);
+        contentValues.put(COL_5,contact);
+        contentValues.put(COL_6,address);
+        contentValues.put(COL_7,e_mail);
+        contentValues.put(COL_8,telephone);
         db.update(TABLE_NAME,contentValues,"id = ?",new String[]{ id } );
         return true;
     }
