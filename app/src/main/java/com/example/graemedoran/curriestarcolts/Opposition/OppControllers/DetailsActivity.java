9be39b888bd3b222f1509af093e20fca92ae3ea9
graeme doran <graeme42@icloud.com>
2017-11-28 01:00:40 +0000

    most thing added now

--- app/src/main/java/com/example/graemedoran/curriestarcolts/Opposition/OppControllers/DetailsActivity.java
+++ app/src/main/java/com/example/graemedoran/curriestarcolts/Opposition/OppControllers/DetailsActivity.java
@@ -1,5 +1,7 @@
 package com.example.graemedoran.curriestarcolts.Opposition.OppControllers;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -7,6 +9,7 @@ import android.widget.Toast;
 
 import com.example.graemedoran.curriestarcolts.CurrieStar.Controllers.MainActivity;
 import com.example.graemedoran.curriestarcolts.Opposition.OppModels.Team;
+import com.example.graemedoran.curriestarcolts.R;
 
 public class DetailsActivity extends MainActivity {
 
@@ -17,31 +20,31 @@ public class DetailsActivity extends MainActivity {
     private Team team;
 
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_profile);
-//        myTeamDb = new OppDatabaseHelper (this);
-//
-//        Intent intent = getIntent();
-//        Bundle extras = intent.getExtras();
-//        team = (Team) extras.get("team");
-//
-//        editTeam_Name = (EditText)findViewById(R.id.editText_name);
-//        editTeam_Name.setText(team.getName());
-//        editContact = (EditText)findViewById(R.id.editText_contact);
-//        editContact.setText(team.getContact());
-//        editAddress = (EditText)findViewById(R.id.editText_address);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        myTeamDb = new OppDatabaseHelper (this);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        team = (Team) extras.get("team");
+
+        editName = (EditText)findViewById(R.id.editText_name);
+        editName.setText(team.getName());
+        editContact = (EditText)findViewById(R.id.editText_contact);
+        editContact.setText(team.getContact());
+        editAddress = (EditText)findViewById(R.id.editText_address);
 //        editAddress.setText(team.getAddress());
-//        editEmail = (EditText)findViewById(R.id.editText_email);
-//        editEmail.setText(team.getE_Mail());
-//        editTel = (EditText)findViewById(R.id.editText_tel);
-//        editTel.setText(team.getTel());
-//        editLocation = (EditText)findViewById(R.id.editText_location);
-//        editLocation.setText(team.getLocation());
-//        btnEdit = (Button)findViewById(R.id.edit_button);
-//        btnDelete = (Button)findViewById(R.id.delete_button);
-//    }
+        editEmail = (EditText)findViewById(R.id.editText_email);
+        editEmail.setText(team.getE_Mail());
+        editTel = (EditText)findViewById(R.id.editText_tel);
+        editTel.setText(team.getTel());
+        editLocation = (EditText)findViewById(R.id.editTextlocation);
+        editLocation.setText(team.getLocation());
+        btnEdit = (Button)findViewById(R.id.edit_button);
+        btnDelete = (Button)findViewById(R.id.delete_button);
+    }
 
     public void deletePlayer(View view){
         Integer deletedRows = myTeamDb.deleteData(Integer.toString(team.getId()));
@@ -53,7 +56,7 @@ public class DetailsActivity extends MainActivity {
 
     }
 //    public void updateDetails(View view) {
-//        String name = editTeam_Name.getText().toString();
+//        String name = editteam_Name.getText().toString();
 //        String contact = editContact.getText().toString();
 //        String address = editAddress.getText().toString();
 //        String email = editEmail.getText().toString();
--- app/src/main/java/com/example/graemedoran/curriestarcolts/Opposition/OppControllers/OpponentsActivity.java
+++ app/src/main/java/com/example/graemedoran/curriestarcolts/Opposition/OppControllers/OpponentsActivity.java
@@ -1,9 +1,13 @@
 package com.example.graemedoran.curriestarcolts.Opposition.OppControllers;
 
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.graemedoran.curriestarcolts.CurrieStar.Controllers.MainActivity;
+import com.example.graemedoran.curriestarcolts.R;
 
 public class OpponentsActivity extends MainActivity {
     OppDatabaseHelper myTeamDb;
@@ -11,37 +15,39 @@ public class OpponentsActivity extends MainActivity {
     Button btnAddNew_Team;
 
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_opponents);
-//        myTeamDb = new OppDatabaseHelper(this);
-//
-//
-//        editTeam_Name = (EditText)findViewById(R.id.input_team_name);
-//        editLocation = (EditText)findViewById(R.id.input_location);
-//        btnAddNew_Team = (Button)findViewById(R.id.button_add_new_team);
-//
-//
-//    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opponents);
+        myTeamDb = new OppDatabaseHelper(this);
 
-//    public void addPlayer(View view) {
-//        String team_name = editTeam_Name.getText().toString();
-//        String location = editLocation.getText().toString();
-//
-//        boolean isInserted = myTeamDb.insertData( team_name, location);
-//        if (isInserted)
-//            Toast.makeText(OpponentsActivity.this, "Data Entered", Toast.LENGTH_SHORT).show();
-//        else
-//            Toast.makeText(OpponentsActivity.this, "Data Not Entered", Toast.LENGTH_SHORT).show();
-//        finish();
-////        if(editText.length() !=0) {
-////            AddData(newEntry);
-////            editText.setText("");
-////        } else {
-////            Toast.makeText(ProfileActivity.this,"All text fields must be filled!", Toast.LENGTH_LONG).show();
-////        }
-//    }
+
+        editTeam_Name = (EditText)findViewById(R.id.new_team_name);
+        editContact = (EditText)findViewById(R.id.contact_name);
+
+        btnAddNew_Team = (Button)findViewById(R.id.add_new_team);
+
+
+    }
+
+    public void addPlayer(View view) {
+        String team_name = editTeam_Name.getText().toString();
+        String contact = editContact.getText().toString();
+
+
+        boolean isInserted = myTeamDb.insertData( team_name, contact);
+        if (isInserted)
+            Toast.makeText(OpponentsActivity.this, "Data Entered", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(OpponentsActivity.this, "Data Not Entered", Toast.LENGTH_SHORT).show();
+        finish();
+//        if(editText.length() !=0) {
+//            AddData(newEntry);
+//            editText.setText("");
+//        } else {
+//            Toast.makeText(ProfileActivity.this,"All text fields must be filled!", Toast.LENGTH_LONG).show();
+//        }
+    }
 }
 
 
